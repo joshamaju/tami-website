06a5b950fb59d22015a205a4b7d37338d45b8b2e
Joshua Amaju <joshsemail4work@gmail.com>
2025-03-24 13:51:01 +0100

    chore

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  outDir: './docs',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
